035d7918937e81591415dd73a10e8905e456eae6
suk-6 <me@suk.kr>
2024-06-02 02:55:14 +0900

    chore: Refactor layout.tsx for improved performance and readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "gopro view",
@@ -19,8 +16,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="ko">
-			<body className={inter.className}>{children}</body>
-			<Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
+			<body>
+				{children}
+				<Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
+			</body>
 		</html>
 	);
 }
