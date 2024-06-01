ca2b369188d135f39af5fda44c0a61c7392558fa
suk-6 <me@suk.kr>
2024-06-02 02:39:01 +0900

    feat: Add KakaoMap component for displaying a map

---
+++ src/components/kakaomap.tsx
@@ -0,0 +1,14 @@
+import { Map } from "react-kakao-maps-sdk";
+
+const KakaoMap = () => {
+	return (
+		<>
+			<Map
+				center={{ lat: 37.55930390626646, lng: 127.04323445018552 }}
+				className=" w-full h-full"
+			></Map>
+		</>
+	);
+};
+
+export default KakaoMap;
