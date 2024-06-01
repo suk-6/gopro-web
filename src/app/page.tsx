ce9d10c7153c87c9a1c10e9c4fb1b4c7c83aadfb
suk-6 <me@suk.kr>
2024-06-02 02:55:23 +0900

    feat: Add KakaoMap component to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import dynamic from "next/dynamic";
+const KakaoMap = dynamic(() => import("@/components/kakaomap"), { ssr: false });
+
 export default function Home() {
-	return <main className=" w-screen h-screen"></main>;
+	return (
+		<main className=" w-screen h-screen">
+			<KakaoMap />
+		</main>
+	);
 }
