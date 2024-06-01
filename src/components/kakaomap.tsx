5e22f5749c9a0720153b0fd4f94857c42d86bd0a
suk-6 <me@suk.kr>
2024-06-02 04:19:37 +0900

    feat: Add lazy loading for images in KakaoMap component

--- src/components/kakaomap.tsx
+++ src/components/kakaomap.tsx
@@ -1,13 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Map } from "react-kakao-maps-sdk";
+import { Road } from "./road";
+import { DataModel } from "@/models/data";
 
 const KakaoMap = () => {
+	const [map, setMap] = useState<kakao.maps.Map | null>(null);
+	const [data, setData] = useState<DataModel | null>(null);
+
+	useEffect(() => {
+		if (!map) return;
+
+		const data = require("/public/result.json");
+		setData(data);
+	}, [map, data]);
+
 	return (
 		<>
 			<Map
 				center={{ lat: 37.55930390626646, lng: 127.04323445018552 }}
 				className=" w-full h-full"
 				level={5}
-			></Map>
+				onCreate={(m) => {
+					setMap(m);
+				}}
+			>
+				{data && <Road points={data.points} />}
+			</Map>
 		</>
 	);
 };
