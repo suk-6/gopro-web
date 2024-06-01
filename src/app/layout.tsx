de8eb381af84dbb641a0638a60205327b1bf778c
suk-6 <me@suk.kr>
2024-06-02 02:39:14 +0900

    feat: Add Kakao SDK script tag to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,8 @@ export const metadata: Metadata = {
 	title: "gopro view",
 	description: "A simple GoPro view app",
 };
+
+const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&autoload=false`;
 
 export default function RootLayout({
 	children,
@@ -17,6 +20,7 @@ export default function RootLayout({
 	return (
 		<html lang="ko">
 			<body className={inter.className}>{children}</body>
+			<Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
 		</html>
 	);
 }
